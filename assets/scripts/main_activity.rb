4b8e2db69782618c96d7f8dcbdd7241779c5496a
myokoym <myokoym@gmail.com>
2011-07-24 13:59:10 +0900

    数値ボタン、入力ボタン、削除ボタン配置

--- assets/scripts/main_activity.rb
+++ assets/scripts/main_activity.rb
@@ -1,21 +1,32 @@
 require 'ruboto'
 
-ruboto_import_widgets :TextView, :LinearLayout, :Button
+ruboto_import_widgets :TextView, :EditText, :LinearLayout, :Button
 
 $activity.handle_create do |bundle|
-  setTitle 'This is the Title'
+  setTitle 'Hit and Blow!'
 
   setup_content do
+    @input_text = ""
     linear_layout :orientation => LinearLayout::VERTICAL do
-      @text_view = text_view :text => "What hath Matz wrought?", :id => 42
-      button :text => "M-x butterfly", :width => :wrap_content, :id => 43
+      @text_view = text_view :text => @input_text, :width => :wrap_content, :id => 42
+      linear_layout :orientation => LinearLayout::HORIZONTAL do
+        0.upto(9) do |i|
+          button :text => "#{i}", :width => :wrap_content, :id => 43 + i
+        end
+      end
+
+      linear_layout :orientation => LinearLayout::HORIZONTAL do
+        button :text => "Submit", :width => :wrap_content, :id => 61
+        button :text => "Delete", :width => :wrap_content, :id => 62
+      end
     end
   end
 
   handle_click do |view|
-    if view.getText == 'M-x butterfly'
-      @text_view.setText "What hath Matz wrought!"
-      toast 'Flipped a bit via butterfly'
+    if /\d+/ =~ view.getText 
+      @input_text << view.getText
+      @text_view.setText @input_text
+      toast 'input!'
     end
   end
 end

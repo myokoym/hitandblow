1165ed1266843bbc95172de89357ecc3c30cc9cd
myokoym <myokoym@gmail.com>
2011-07-24 22:50:01 +0900

    ボタン位置調整中

--- assets/scripts/main_activity.rb
+++ assets/scripts/main_activity.rb
@@ -4,34 +4,36 @@ ruboto_import_widgets :TextView, :EditText, :LinearLayout, :Button
 
 $activity.handle_create do |bundle|
   setTitle 'Hit and Blow!'
+  CELL_SIDE = 70
+  CELL_HEIGHT = 40
 
   setup_content do
     @answer = Array.new(4) { rand(10).to_s }
     @input = []
     linear_layout :orientation => LinearLayout::VERTICAL do
-      @input_view = text_view :text => @input.join, :width => :wrap_content, :id => 42
+      @input_view = edit_text :text => @input.join, :width => :wrap_content, :id => 42
       linear_layout :orientation => LinearLayout::HORIZONTAL do
-        button :text => "1", :width => 70, :height => 70, :id => 51
-        button :text => "2", :width => 70, :height => 70, :id => 52
-        button :text => "3", :width => 70, :height => 70, :id => 53
+        button :text => "1", :width => CELL_SIDE, :height => CELL_SIDE, :id => 51
+        button :text => "2", :width => CELL_SIDE, :height => CELL_SIDE, :id => 52
+        button :text => "3", :width => CELL_SIDE, :height => CELL_SIDE, :id => 53
       end
 
       linear_layout :orientation => LinearLayout::HORIZONTAL do
-        button :text => "4", :width => 70, :height => 70, :id => 54
-        button :text => "5", :width => 70, :height => 70, :id => 55
-        button :text => "6", :width => 70, :height => 70, :id => 56
+        button :text => "4", :width => CELL_SIDE, :height => CELL_SIDE, :id => 54
+        button :text => "5", :width => CELL_SIDE, :height => CELL_SIDE, :id => 55
+        button :text => "6", :width => CELL_SIDE, :height => CELL_SIDE, :id => 56
       end
 
       linear_layout :orientation => LinearLayout::HORIZONTAL do
-        button :text => "7", :width => 70, :height => 70, :id => 57
-        button :text => "8", :width => 70, :height => 70, :id => 58
-        button :text => "9", :width => 70, :height => 70, :id => 59
+        button :text => "7", :width => CELL_SIDE, :height => CELL_SIDE, :id => 57
+        button :text => "8", :width => CELL_SIDE, :height => CELL_SIDE, :id => 58
+        button :text => "9", :width => CELL_SIDE, :height => CELL_SIDE, :id => 59
       end
 
       linear_layout :orientation => LinearLayout::HORIZONTAL do
-        button :text => "Delete", :width => 70, :height => 40, :id => 62
-        button :text => "0", :width => 70, :height => 70, :id => 50
-        button :text => "Submit", :width => 70, :height => 40,:id => 61
+        button :text => "Submit", :width => CELL_SIDE, :height => CELL_HEIGHT,:id => 61
+        button :text => "0", :width => CELL_SIDE, :height => CELL_SIDE, :id => 50
+        button :text => "Delete", :width => CELL_SIDE, :height => CELL_HEIGHT, :id => 62
       end
 
       linear_layout :orientation => LinearLayout::VERTICAL do
@@ -56,6 +58,8 @@ $activity.handle_create do |bundle|
       blow = blow - hit
       @answer_view.setText @input.join
       @hit_blow_view.setText "hit: #{hit}, blow: #{blow}"
+      @input = []
+      @input_view.setText @input.join
     end
   end
 end
